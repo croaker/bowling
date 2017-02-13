3ed4b01b765da1576a29f84a7fe977087861fd80
Daniel Gruenthal <d.gruenthal@gmail.com>
2017-02-13 19:30:32 +0100

    Yay, shop talks done :)

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,39 @@
 class Bowling
+  def initialize
+    @rolls = []
+  end
 
+  def roll(pins)
+    @rolls << pins
+  end
+
+  def score
+    score = 0
+    frame_index = 0
+
+    10.times { |frame|
+      if spare?(frame_index)
+        score += 10 + @rolls[frame_index + 2]
+        frame_index += 2
+      elsif @rolls[frame_index] == 10
+        score += 10 +
+                 @rolls[frame_index + 1] +
+                 @rolls[frame_index + 2]
+        frame_index += 1
+      else
+        score += score_pins_in_frame(frame_index)
+        frame_index += 2
+      end
+    }
+
+    score
+  end
+
+  def score_pins_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def spare?(frame_index)
+    score_pins_in_frame(frame_index) == 10
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,8 +1,58 @@
 require_relative "../lib/bowling"
 
 RSpec.describe Bowling do
+  let(:game) { Bowling.new }
+
+  def roll_many(count:, pins:)
+    count.times { game.roll(pins) }
+  end
+
+  def roll_spare
+    game.roll(5)
+    game.roll(5)
+  end
+
   it "can score a gutter game" do
-    bowling = Bowling.new
-    10.times { bowling.roll(0) }
+    roll_many(count: 20, pins: 0)
+
+    expect(game.score).to eq 0
+  end
+
+  it "can score an all 1s game" do
+    roll_many(count: 20, pins: 1)
+
+    expect(game.score).to eq 20
+  end
+
+  it "can score one spare" do
+    roll_spare
+    game.roll(3)
+    roll_many(count: 17, pins: 0)
+
+    expect(game.score).to eq 16
+  end
+
+  it "can score a strike" do
+    game.roll(10) # strike
+    game.roll(3)
+    game.roll(3)
+    roll_many(count: 17, pins: 0)
+
+    expect(game.score).to eq 22
+  end
+
+  it "can score a perfect game" do
+    roll_many(count: 12, pins: 10)
+
+    expect(game.score).to eq 300
+  end
+
+  it "can score a nearly perfect game" do
+    roll_many(count: 9, pins: 10)
+    roll_spare
+    game.roll(0)
+
+    expect(game.score).to eq 265
+
   end
 end
